70f4d929b332a43a0eb90ced7022f04d3d4c84c7
Geraud-Vercasson <geraud.vercasson@gmail.com>
2018-05-14 14:57:30 +0200

    donation >= 100 and integer

--- app/DonationFee.php
+++ app/DonationFee.php
@@ -9,6 +9,8 @@
 namespace App;
 
 
+use Mockery\Exception;
+
 class DonationFee
 {
 
@@ -17,6 +19,17 @@ class DonationFee
 
     public function __construct($donation, $commissionPercentage)
     {
+        if ($commissionPercentage < 0 || $commissionPercentage > 30) {
+            throw new Exception('Commission percentage out of bounds dude!');
+        }
+
+        if ($donation < 100) {
+            throw new Exception('Donation must be at least 100 cents dumbass!');
+        }
+
+        if (!is_int($donation)) {
+            throw new Exception('Donation must be an integer, bro!');
+        }
         $this->donation = $donation;
         $this->commissionPercentage = $commissionPercentage;
     }
@@ -26,5 +39,10 @@ class DonationFee
     public function getCommissionAmount()
     {
         return ($this->commissionPercentage/100) * $this->donation;
+    }
+
+    public function getAmountCollected()
+    {
+        return $this->donation * (1 - ($this->commissionPercentage/100));
     }
 }
